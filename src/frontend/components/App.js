5cdc5041ee6bc593fe26831f1c9357aa9c907e9d
Kyle Oliveri <kyle@Kyles-MacBook-Air.local>
2022-05-09 17:10:11 -0700

    Added js logic to app.js if the metamask wallet has not been loaded

--- src/frontend/components/App.js
+++ src/frontend/components/App.js
@@ -1,75 +1,80 @@
-
-import logo from './logo.png';
-import './App.css';
-import Navigation from './Navbar';
-import { useState } from 'react';
+import logo from "./logo.png";
+import "./App.css";
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ethers } from "ethers";
-import MarketplaceAbi from '../contractsData/Marketplace.json'
-import MarketplaceAddress from '../contractsData/Marketplace-address.json'
-import NFTAbi from '../contractsData/NFT.json'
-import NFTAddress from '../contractsData/NFT-address.json'
- 
+import { Spinner } from 'react-bootstrap'
+import Navigation from "./Navbar";
+import Home from "./Home";
+import Create from "./Create";
+import MyListedItems from "./MyListedItems";
+import MyPurchases from "./MyPurchases";
+import MarketplaceAbi from "../contractsData/Marketplace.json";
+import MarketplaceAddress from "../contractsData/Marketplace-address.json";
+import NFTAbi from "../contractsData/NFT.json";
+import NFTAddress from "../contractsData/NFT-address.json";
+
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [account, setAccount] = useState(null);
+  const [marketplace, setMarketplace] = useState({});
+  const [nft, setNFT] = useState({});
 
-const [loading, setLoading] = useState(true)
-const [account, setAccount] = useState(null)
-const [marketplace, setMarketplace] = useState({})
-const [nft, setNFT] = useState({})
+  // MetaMask Login/Connect
+  const web3Handler = async () => {
+    const accounts = await window.ethereum.request({
+      method: "eth_requestAccounts",
+    });
+    setAccount(accounts[0]);
 
-// MetaMask Login/Connect
-const web3Handler = async () => {
-  const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-  setAccount(accounts[0]);
+    // Get provider from MetaMask
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
 
-  // Get provider from MetaMask
-  const provider = new ethers.providers.Web3Provider(window.ethereum);
+    // Get signer
+    const signer = provider.getSigner();
 
-   // Get signer
-   const signer = provider.getSigner();
+    loadContracts(signer);
+  };
 
-   loadContracts(signer);
-}
-
-const loadContracts = async (signer) => {
-  //Get deployed copies of contracts
-  const marketplace = new ethers.Contract(MarketplaceAddress.address, MarketplaceAbi.abi, signer);
-  setMarketplace(marketplace);
-  const nft = new ethers.Contract(NFTAddress.address, NFTAbi.abi, signer)
-  setNFT(nft);
-  setLoading(false);
-}
+  const loadContracts = async (signer) => {
+    //Get deployed copies of contracts
+    const marketplace = new ethers.Contract(
+      MarketplaceAddress.address,
+      MarketplaceAbi.abi,
+      signer
+    );
+    setMarketplace(marketplace);
+    const nft = new ethers.Contract(NFTAddress.address, NFTAbi.abi, signer);
+    setNFT(nft);
+    setLoading(false);
+  };
 
   return (
-    <div>
-      <Navigation web3Handler={web3Handler} account={account} />
-      <div className="container-fluid mt-5">
-        <div className="row">
-          <main role="main" className="col-lg-12 d-flex text-center">
-            <div className="content mx-auto mt-5">
-              <a
-                href="#"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <img src={logo} className="App-logo" alt="logo"/>
-              </a>
-              <h1 className= "mt-5">Dapp Starter Kit</h1>
-              <p>
-                Edit <code>src/frontend/components/App.js</code> and save to reload.
-              </p>
-              <a
-                className="App-link"
-                href="#"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                LEARN BLOCKCHAIN <u><b>NOW! </b></u>
-              </a>
-            </div>
-          </main>
-        </div>
+    <BrowserRouter>
+      <div>
+        <Navigation web3Handler={web3Handler} account={account} />
+        {loading ? (
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              minHeight: "80vh",
+            }}
+          >
+            <Spinner animation="border" style={{ display: "flex" }} />
+            <p className='mx-3 my-0'>Awaiting MetaMask Connection...</p>
+          </div>
+        ) : (
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/create" element={<Create />} />
+            <Route path="my-listed-items" element={<MyListedItems />} />
+            <Route path="/my-purchases" element={<MyPurchases />} />
+          </Routes>
+        )}
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
--- src/frontend/components/Navbar.js
+++ src/frontend/components/Navbar.js
@@ -13,17 +13,17 @@ const Navigation = ({ web3Handler, account }) => {
                     <Navbar.Toggle aria-controls='responsive-navbar-nav' />
                         <Navbar.Collapse id='responsive-navbar-nav'>
                             <Nav className='me-auto'>
-                                <Nav.Link href='/'>Home</Nav.Link>
-                                <Nav.Link href='/create'>Create</Nav.Link>
-                                <Nav.Link href='/my-listed-items'>My Listed Items</Nav.Link>
-                                <Nav.Link href='/my-purchases'>My Purchases</Nav.Link>
+                                <Nav.Link as={Link} to='/'>Home</Nav.Link>
+                                <Nav.Link as={Link} to='/create'>Create</Nav.Link>
+                                <Nav.Link as={Link} to='/my-listed-items'>My Listed Items</Nav.Link>
+                                <Nav.Link as={Link} to='/my-purchases'>My Purchases</Nav.Link>
                             </Nav>
                             <Nav>
                                 {account ? (
                                     <Button href={`https://etherscan.io/address/${account}`} target='_blank' rel='noopener noreferrer' className='button nav-button btn-sm mx-4'>
-                                        <p variant='outline-light'>
+                                        <span variant='outline-light'>
                                             {account.slice(0, 5) + '...' + account.slice(38, 42)}
-                                        </p>
+                                        </span>
                                     </Button>
                                 ) : (
                                     <Button onClick={web3Handler} variant='outline-light'>Connect Wallet</Button>
